bacf43fb206d8d6d0f3f1c3c5d9700f121a0c124
Eric Jonese <ericnatejones@gmail.com>
2017-09-27 15:19:30 -0600

    if statements instead of ternarys

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ var mouse = {x: 0, y: 0};
 
 document.addEventListener("mousemove", (event)=>{
 
-  event.y > 300 ? mouse.y = 300: mouse.y = event.y;
-  event.y < 100 ? mouse.y = 100: mouse.y = event.y;
+    if(event.y > 300){
+        mouse.y = 300;
+    } else {
+        mouse.y = event.y;
+    }
 
-  event.x > 300 ? mouse.x = 300: mouse.x = event.x;
-  event.x < 100 ? mouse.x = 100: mouse.x = event.x;
+    if (event.y < 100) {
+        mouse.y = 100;
+    } else {
+        mouse.y = event.y;
+    }
 
-  document.getElementById("number").innerHTML = "x: " + mouse.x + "<br>y: " + mouse.y
+    if(event.x > 300){
+        mouse.x = 300;
+    } else {
+        mouse.x = event.x;
+    }
+
+    if (event.x < 100){
+        mouse.x = 100;
+    } else {
+        mouse.x = event.x;
+    }
+
+    document.getElementById("number").innerHTML = "x: " + mouse.x + "<br>y: " + mouse.y
 
 });
